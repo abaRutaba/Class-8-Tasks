62955aa602e0ce26fd7807b401179c9aa658f178
Rutaba Shamaim <rutabash@yahoo.com>
2018-07-24 10:54:19 +0500

    1st commit

---
+++ Salary/Salary.js
@@ -0,0 +1,33 @@
+var app = angular.module("mainApp",[]);
+//...... controller
+app.controller('calSalary',function($scope, calService)
+{
+    $scope.cal = function()
+    {
+        $scope.gSalary = calService.gross(parseInt($scope.basicSal),parseInt($scope.houseAll),parseInt( $scope.tax), parseInt($scope.medicalAll),parseInt($scope.transport))
+        console.log("controller running")
+    }
+})
+//........Service
+app.service('calService', function(salaryFactory)
+{
+    this.gross = function(basic,house, tax, medical, transport)
+    {
+        console.log("service running")
+        sum = basic+house+medical+transport
+        return salaryFactory.total(sum,tax)
+    }
+})
+//........Factory
+app.factory('salaryFactory', function()
+    {
+        console.log("factory running")
+        var factory = {}
+        factory.total = function(pay, Tax)
+        {
+            console.log("pay ="+pay)
+            console.log("tax="+Tax)
+            return (pay-Tax);
+        }
+        return factory
+    })
